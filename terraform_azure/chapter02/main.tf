fa3338ba669db7a93b29d58c46c276b03df45714
mervyn <mervyn.vangompel@gmail.com>
2025-09-15 18:43:21 +0200

    resolving ssh issue

--- terraform_azure/chapter02/main.tf
+++ terraform_azure/chapter02/main.tf
@@ -108,7 +108,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   admin_ssh_key {
     username   = "azureuser"
-    public_key = file("~/.ssh/id_rsa.pub")
+    public_key = file("${path.module}/id_rsa.pub")
   }
 
   os_disk {
